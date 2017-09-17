c1e5e783ab5dc36bfe91c96f87a086bee2e99589
dia.pasc <dia.pasc@gmail.com>
2017-09-17 18:43:19 +0300

    first commit

---
+++ AncientStories/Controllers/StoriesController.cs
@@ -0,0 +1,125 @@
+﻿using AncientStories.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace AncientStories.Controllers
+{
+    public class StoriesController : Controller
+    {
+        private StoryDBContext db = new StoryDBContext();
+
+        // GET: Stories
+        public ActionResult Index()
+        {
+            return View(db.Stories.ToList());
+        }
+
+        // GET: Stories/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Story story = db.Stories.Find(id);
+            if (story == null)
+            {
+                return HttpNotFound();
+            }
+            return View(story);
+        }
+
+        // GET: Stories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Stories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Civilization,Period,Description")] Story story)
+        {
+            if (ModelState.IsValid)
+            {
+                story.Id = Guid.NewGuid();
+                db.Stories.Add(story);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(story);
+        }
+
+        // GET: Stories/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Story story = db.Stories.Find(id);
+            if (story == null)
+            {
+                return HttpNotFound();
+            }
+            return View(story);
+        }
+
+        // POST: Stories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Civilization,Period,Description")] Story story)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(story).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(story);
+        }
+
+        // GET: Stories/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Story story = db.Stories.Find(id);
+            if (story == null)
+            {
+                return HttpNotFound();
+            }
+            return View(story);
+        }
+
+        // POST: Stories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Story story = db.Stories.Find(id);
+            db.Stories.Remove(story);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AncientStories/Models/Story.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace AncientStories.Models
+{
+    public class Story
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Civilization { get; set; }
+        public string Period { get; set; }  
+        public string Description { get; set; }
+    }
+
+    public class StoryDBContext : DbContext
+    {
+        public DbSet<Story> Stories { get; set; }
+    }
+}
+
+
